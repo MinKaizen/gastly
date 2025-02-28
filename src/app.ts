2e3c72331b5ad9a960f5685bdcbb00b280a9642c
minkaizen <martin.g.cao@gmail.com>
2025-02-28 19:20:10 +1100

    feat: implement hello world menu item

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
+function onOpen(): void {
+  SpreadsheetApp.getUi().createMenu('Hello')
+    .addItem('Hello', 'hello')
+    .addToUi()
+}
+
 function hello(): void {
   Browser.msgBox('Hello world!')
 }
